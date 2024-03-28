c1119be8b316960c5bd40eaa2c6e3bf1abb8fc1e
Daarkosss <karolpilat59@gmail.com>
2024-03-28 18:49:48 +0100

    Easter card v3

--- lib/easter_card.dart
+++ lib/easter_card.dart
@@ -3,6 +3,7 @@ import 'animated_text.dart';
 import 'animated_bunny.dart';
 import 'animated_chicken.dart';
 import 'animated_egg.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class EasterCard extends StatefulWidget {
   const EasterCard({Key? key}) : super(key: key);
@@ -13,6 +14,7 @@ class EasterCard extends StatefulWidget {
 
 class _EasterCardState extends State<EasterCard> with TickerProviderStateMixin {
   late AnimationController _controller;
+  final audioPlayer = AudioPlayer(); // Dodaj tę linię
   List<Widget> chickens = [];
 
   @override
@@ -22,8 +24,22 @@ class _EasterCardState extends State<EasterCard> with TickerProviderStateMixin {
       duration: const Duration(seconds: 10),
       vsync: this,
     );
-    _controller.forward();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      startAnimationsAndSound(); // Zakładam, że to uruchamia animacje i dźwięk.
+    });
     chickens.add(AnimatedChicken(key: UniqueKey()));
+  }
+
+  void startAnimationsAndSound() {
+    playSound(); // Odtwarzanie dźwięku
+    _controller.forward(); // Rozpoczęcie animacji
+    // Tutaj możesz również dodać logikę do rozpoczęcia animacji dla każdego kurczaka, jeśli mają własne kontrolery.
+  }
+
+  Future<void> playSound() async {
+    final player = AudioCache(prefix: 'assets/audio/');
+    final url = await player.load('music.mp3');
+    audioPlayer.play(url.path, isLocal: true);
   }
 
   void addChicken() {
@@ -42,38 +58,46 @@ class _EasterCardState extends State<EasterCard> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/background.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Stack(
-          children: const <Widget>[
-            AnimatedEgg(
-              margin: EdgeInsets.only(right: 100),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/background.jpg"),
+                fit: BoxFit.cover,
+              ),
             ),
-            AnimatedEgg(
-              margin: EdgeInsets.only(right: 1500),
-            ),            
-            JumpingBunny(offset: 0, delay: 0),
-            JumpingBunny(offset: 200, delay: 1500),
-            JumpingBunny(offset: 400, delay: 0),
-            JumpingBunny(offset: 600, delay: 1500),
-            JumpingBunny(offset: 800, delay: 0),
-            JumpingBunny(offset: 1000, delay: 1500),
-            JumpingBunny(offset: 1200, delay: 0),
-            JumpingBunny(offset: 1400, delay: 1500),
-            JumpingBunny(offset: 1600, delay: 0),
-            AnimatedTextWidget()
-          ] + chickens,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: addChicken,
-        child: Icon(Icons.add),
-      ),
+            child: Stack(
+              children: const <Widget>[
+                AnimatedEgg(
+                  margin: EdgeInsets.only(right: 100),
+                ),
+                AnimatedEgg(
+                  margin: EdgeInsets.only(right: 1500),
+                ),            
+                JumpingBunny(offset: 0, delay: 0),
+                JumpingBunny(offset: 200, delay: 1500),
+                JumpingBunny(offset: 400, delay: 0),
+                JumpingBunny(offset: 600, delay: 1500),
+                JumpingBunny(offset: 800, delay: 0),
+                JumpingBunny(offset: 1000, delay: 1500),
+                JumpingBunny(offset: 1200, delay: 0),
+                JumpingBunny(offset: 1400, delay: 1500),
+                JumpingBunny(offset: 1600, delay: 0),
+                AnimatedTextWidget()
+              ] + chickens,
+            ),
+          ),
+          Positioned(
+            right: 30,
+            bottom: 80,
+            child: FloatingActionButton(
+              onPressed: addChicken,
+              child: Icon(Icons.add),
+            ),
+          ),
+        ]
+      )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
 import 'easter_card.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.lightGreen[100],
-        body: const EasterCard(),
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreen[100],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Naciśnij przycisk, aby odtworzyć kartkę wielkanocną!',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            FloatingActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => EasterCard()),
+                );
+              },
+              child: Icon(Icons.play_arrow),
+              tooltip: 'Start',
+            ),
+          ],
+        ),
       ),
     );
   }
